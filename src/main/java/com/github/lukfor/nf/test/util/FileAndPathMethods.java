d46bb56bae776ecd0ad3bfc255d79d087f22677c
Lukas Forer <lukas.forer@i-med.ac.at>
2021-10-24 17:11:04 +0200

    Add with method to improve readability

--- src/main/java/com/github/lukfor/nf/test/util/FileAndPathMethods.java
+++ src/main/java/com/github/lukfor/nf/test/util/FileAndPathMethods.java
@@ -3,6 +3,8 @@ package com.github.lukfor.nf.test.util;
 import java.io.File;
 import java.nio.file.Path;
 
+import groovy.lang.Closure;
+
 public class FileAndPathMethods {
 
 	public static File file(String filename) {
@@ -11,5 +13,11 @@ public class FileAndPathMethods {
 
 	public static Path path(String filename) {
 		return Path.of(filename);
+	}
+	
+	public static void with(Object context, Closure closure) {
+		closure.setDelegate(context);
+		closure.setResolveStrategy(Closure.DELEGATE_FIRST);
+		closure.call();
 	}
 }
