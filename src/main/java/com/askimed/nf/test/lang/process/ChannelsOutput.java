976c50e192127a171017362b2f5dd8accefb94e3
Lukas Forer <lukas.forer@i-med.ac.at>
2021-11-03 11:47:44 +0100

    Simplify sort of channels and support tuples

--- src/main/java/com/askimed/nf/test/lang/process/ChannelsOutput.java
+++ src/main/java/com/askimed/nf/test/lang/process/ChannelsOutput.java
@@ -2,7 +2,6 @@ package com.askimed.nf.test.lang.process;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -30,8 +29,7 @@ public class ChannelsOutput extends HashMap<Object, Object> {
 			for (Object key : map.keySet()) {
 				Object value = map.get(key);
 				List<Object> channel = (List<Object>) value;
-				List<? extends Object> sortedChannel = sortChannel(channel);
-				map.put(key, sortedChannel);
+				sortChannel(channel);
 			}
 		}
 		return map;
@@ -42,82 +40,74 @@ public class ChannelsOutput extends HashMap<Object, Object> {
 				.println(AnsiText.padding(groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(this)), 4));
 	}
 
-	public List<? extends Object> sortChannel(List<Object> channel) {
-		Class channelClass = detectClassOfChannel(channel);
-		if (channelClass == String.class) {
-			if (areItemsPaths(channel)) {
-				return sortPathChannel(channel);
-			} else {
-				return sortStringChannel(channel);
-			}
-		}
-		if (channelClass == Integer.class) {
-			return sortNumberChannel(channel);
-		}
-		if (channelClass == Double.class) {
-			return sortNumberChannel(channel);
-		}
-		if (channelClass == Float.class) {
-			return sortNumberChannel(channel);
-		}
-		return channel;
-	}
+	public void sortChannel(List<Object> channel) {
+		channel.sort(new Comparator<Object>() {
 
-	public List<String> sortStringChannel(List<Object> channel) {
-		List<String> newChannel = new ArrayList<String>();
-		for (Object item : channel) {
-			newChannel.add((String) item);
-		}
-		Collections.sort(newChannel);
-		return newChannel;
-	}
-
-	public List<String> sortPathChannel(List<Object> channel) {
-		List<String> newChannel = new ArrayList<String>();
-		for (Object item : channel) {
-			newChannel.add((String) item);
-		}
-		Collections.sort(newChannel, new Comparator<String>() {
 			@Override
-			public int compare(String o1, String o2) {
-				String name1 = new File(o1).getName();
-				String name2 = new File(o2).getName();
-				return name1.compareTo(name2);
-			}
-		});
-		return newChannel;
-	}
+			public int compare(Object o1, Object o2) {
 
-	public List<Comparable> sortNumberChannel(List<Object> channel) {
-		List<Comparable> newChannel = new ArrayList<Comparable>();
-		for (Object item : channel) {
-			newChannel.add((Comparable) item);
-		}
-		Collections.sort(newChannel);
-		return newChannel;
-	}
+				if (o1 instanceof ArrayList) {
 
-	public Class detectClassOfChannel(List<Object> channel) {
-		Class clazz = null;
-		for (Object item : channel) {
-			if (clazz == null) {
-				clazz = item.getClass();
-			} else {
-				if (clazz != item.getClass()) {
-					return Object.class;
+					ArrayList<Object> tuple1 = (ArrayList<Object>) o1;
+					ArrayList<Object> tuple2 = (ArrayList<Object>) o2;
+					for (int i = 0; i < tuple1.size(); i++) {
+						Object a = tuple1.get(i);
+						Object b = tuple2.get(i);
+
+						int result = compareObjects(a, b);
+						
+						if (result != 0) {
+							return result;
+						}
+
+					}
+				} else {
+
+					return compareObjects(o1, o2);
+
 				}
+
+				return 0;
 			}
-		}
-		return clazz;
+
+		});
 	}
 
-	public boolean areItemsPaths(List<Object> channel) {
-		for (Object item : channel) {
-			if (!item.toString().startsWith("/")) {
-				return false;
-			}
+	public int compareObjects(Object a, Object b) {
+
+		// TODO: check classA == classB
+
+		if (a.getClass() != b.getClass()) {
+			System.out.println("Warning: Items with different classes: " + a.getClass() + " vs. " + b.getClass());
+			return 1;
 		}
-		return true;
+
+		if (a instanceof String) {
+			if (a.toString().startsWith("/")) {
+				return comparePaths(a.toString(), b.toString());
+			} else {
+				return compareStrings(a.toString(), b.toString());
+			}
+		} else if (a instanceof Integer || a instanceof Double || a instanceof Float) {
+			return compareNumbers((Comparable) a, (Comparable) b);
+		} else {
+			System.out.println("Warning: Unsupported classes: " + a.getClass() + " vs. " + b.getClass());
+			return 1;
+		}
+
 	}
 
+	public int comparePaths(String a, String b) {
+		String name1 = new File(a).getName();
+		String name2 = new File(b).getName();
+		return name1.compareTo(name2);
+	}
+
+	public int compareStrings(String a, String b) {
+		return a.compareTo(b);
+	}
+
+	public int compareNumbers(Comparable a, Comparable b) {
+		return a.compareTo(b);
+	}
 }
