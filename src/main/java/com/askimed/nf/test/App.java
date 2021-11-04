f0564bbbd50cf966f3af48d3c7a71a06f9440b9d
Lukas Forer <lukas.forer@i-med.ac.at>
2021-11-04 15:26:11 +0100

    Prepare release 0.2.1

--- src/main/java/com/askimed/nf/test/App.java
+++ src/main/java/com/askimed/nf/test/App.java
@@ -14,7 +14,7 @@ public class App {
 
 	public static final String NAME = "nf-test";
 
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.2.1";
 
 	public int run(String[] args) {
 
