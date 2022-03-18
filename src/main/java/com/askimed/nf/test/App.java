5d536caf8762b977c6d54380d71a440a677d92ec
Lukas Forer <lukas.forer@i-med.ac.at>
2022-03-18 14:26:46 +0100

    Fix typo in copyright

--- src/main/java/com/askimed/nf/test/App.java
+++ src/main/java/com/askimed/nf/test/App.java
@@ -39,7 +39,7 @@ public class App {
 		System.out.println();
 		System.out.println(Emoji.ROCKET + AnsiText.bold(" " + App.NAME + " " + App.VERSION));
 		System.out.println("https://code.askimed.com/nf-test");
-		System.out.println("(c) 2021 -2022 Lukas Forer and Sebastian Schoenherr");
+		System.out.println("(c) 2021 - 2022 Lukas Forer and Sebastian Schoenherr");
 		System.out.println();
 
 	}
