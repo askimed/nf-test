dc58d880302d7c66767237bd391258a1c76dfedc
Lukas Forer <lukas.forer@i-med.ac.at>
2022-04-16 11:18:53 +0200

    Use Pats.get to be compatible with Java 8

--- src/main/java/com/askimed/nf/test/util/FileAndPathMethods.java
+++ src/main/java/com/askimed/nf/test/util/FileAndPathMethods.java
@@ -2,6 +2,7 @@ package com.askimed.nf.test.util;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import groovy.lang.Closure;
 
@@ -12,7 +13,7 @@ public class FileAndPathMethods {
 	}
 
 	public static Path path(String filename) {
-		return Path.of(filename);
+		return Paths.get(filename);
 	}
 	
 	public static void with(Object context, Closure closure) {
