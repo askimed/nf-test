f1343ebd948ff83a2a2717993c21209553d40a15
Lukas Forer <lukas.forer@i-med.ac.at>
2022-10-10 19:36:40 +0200

    Make when optional in `FunctionTest`

--- src/main/java/com/askimed/nf/test/lang/function/FunctionTest.java
+++ src/main/java/com/askimed/nf/test/lang/function/FunctionTest.java
@@ -108,7 +108,9 @@ public class FunctionTest extends AbstractTest {
 			setup.execute(context);
 		}
 
-		when.execute(context);
+		if (when != null) {		
+			when.execute(context);
+		}
 
 		context.evaluateParamsClosure(baseDir, outputDir.getAbsolutePath());
 		context.evaluateFunctionClosure();
