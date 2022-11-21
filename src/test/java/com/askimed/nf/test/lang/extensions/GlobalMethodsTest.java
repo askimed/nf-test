c9954631dafb852902d169b436c52fc773b02c93
aaron-fishman-achillestx <114482275+aaron-fishman-achillestx@users.noreply.github.com>
2022-11-21 19:17:50 +0000

    Assert in any order (#61)
    * assertListUnsorted insensitive to frequency
    
    * assertListUnsorted with counts
    
    * example
    
    * auto-parse
    
    * throwing a groovy error
    
    * cleaning up
    
    * preparing GlobalMethodsTest
    
    * success cases
    
    * groovy test cases
    
    * updating docs
    
    * updating docs
    
    * docs
    
    * removing dev code
    
    * throws delaration
    
    * Update assertions.md
    
    * improving the docs

--- src/test/java/com/askimed/nf/test/lang/extensions/GlobalMethodsTest.java
+++ src/test/java/com/askimed/nf/test/lang/extensions/GlobalMethodsTest.java
@@ -1,20 +1,25 @@
 package com.askimed.nf.test.lang.extensions;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
 
 import org.codehaus.groovy.control.customizers.ImportCustomizer;
 import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.runtime.powerassert.PowerAssertionError;
 
 import groovy.lang.GroovyShell;
 
 public class GlobalMethodsTest {
+    private GroovyShell shell;
 
-    @Test
-    public void testAssertAll() throws Exception {
+    // Setup
+	@BeforeEach
+	public void setUp() throws IOException {
         // Prepare groovy shell with GlobalMethods class
         ImportCustomizer customizer = new ImportCustomizer();
         customizer.addStaticStars("com.askimed.nf.test.lang.extensions.GlobalMethods");
@@ -22,14 +27,34 @@ public class GlobalMethodsTest {
         CompilerConfiguration compilerConfiguration = new CompilerConfiguration();
         compilerConfiguration.addCompilationCustomizers(customizer);
 
-        GroovyShell shell = new GroovyShell(compilerConfiguration);
+        shell = new GroovyShell(compilerConfiguration);
+	}
 
+
+    @Test
+    public void testAssertAll() throws Exception {
         // Run groovy script
-        Exception exception = assertThrows(Exception.class, () -> {
+        Throwable thrown = assertThrows(PowerAssertionError.class, () -> {
             Object object = shell.evaluate(new File("./test-data/assertAll.groovy"));
         });
 
         // Assert the message
-        assertTrue(exception.toString().contains("3 of 4 assertions failed"));
+        assertTrue(thrown.toString().contains("3 of 4 assertions failed"));
     }
+
+    // Add test for assertContainsInAnyOrder
+    @Test
+    public void testAssertInAnyOrderSuccessfulCases() throws Exception {
+        // Run groovy script
+        shell.evaluate(new File("./test-data/assertInAnyOrderSuccessfulCases.groovy"));
+    }
+
+    @Test
+    public void testAssertInAnyOrderFailureCases() throws Exception {
+        // Run groovy script
+        Throwable thrown = assertThrows(PowerAssertionError.class, () -> {
+            Object object = shell.evaluate(new File("./test-data/assertInAnyOrderFailureCases.groovy"));
+        });
+    }
+
 }
