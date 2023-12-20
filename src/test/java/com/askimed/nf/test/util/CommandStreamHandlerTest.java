4813f0a8fe9da52470b4c47b8c9015a91427fa11
Arthur Vigil <arthur.vigil@invitae.com>
2023-12-20 15:58:07 -0700

    CommandStreamHandler test case

---
+++ src/test/java/com/askimed/nf/test/util/CommandStreamHandlerTest.java
@@ -0,0 +1,60 @@
+package com.askimed.nf.test.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+public class CommandStreamHandlerTest {
+
+	@Test
+	public void testCommandStreamHandler() {
+		// capture stdout
+		PrintStream stdout = System.out;
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        String commandOutput =
+            "------------------------------------------------------\n"
+          + "If you use nf-core/rnaseq for your analysis please cite:\n"
+          + "\n"
+          + "* The pipeline\n"
+          + "  https://doi.org/10.5281/zenodo.1400710\n"
+          + "\n"
+          + "* The nf-core framework\n"
+          + "  https://doi.org/10.1038/s41587-020-0439-x\n"
+          + "\n"
+          + "* Software dependencies\n"
+          + "  https://github.com/nf-core/rnaseq/blob/master/CITATIONS.md\n"
+          + "------------------------------------------------------\n";
+
+        ByteArrayInputStream is = new ByteArrayInputStream(commandOutput.getBytes());
+		CommandStreamHandler handler = new CommandStreamHandler((InputStream) is);
+		handler.run();
+
+        String expectedOutput =
+            "    > ------------------------------------------------------\n"
+          + "    > If you use nf-core/rnaseq for your analysis please cite:\n"
+          + "    > \n"
+          + "    > * The pipeline\n"
+          + "    >   https://doi.org/10.5281/zenodo.1400710\n"
+          + "    > \n"
+          + "    > * The nf-core framework\n"
+          + "    >   https://doi.org/10.1038/s41587-020-0439-x\n"
+          + "    > \n"
+          + "    > * Software dependencies\n"
+          + "    >   https://github.com/nf-core/rnaseq/blob/master/CITATIONS.md\n"
+          + "    > ------------------------------------------------------\n";
+
+		assertEquals(
+		    expectedOutput,
+		    out.toString()
+		);
+
+		System.setOut(stdout);
+	}
+}
